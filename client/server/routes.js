8da449b4b2de4636ced13d19a10091b2fed89d0d
Nick95 <nicolae.botnaru@gmail.com>
2017-12-20 13:45:50 +0200

    added some changes

--- client/server/routes.js
+++ client/server/routes.js
@@ -7,14 +7,6 @@ function Routes (app) {
         res.sendFile(path.resolve(__dirname + './../public/index.html'));
     });
 
-    app.get('/test', function (req, res) {
-        res.send('da');
-    })
-
-    var mainPage = require('./app/ctrls/main-page/main-page.js');
-    app.get('/basic-info', mainPage.getBasicInfoApi);
-    app.post('/basic-info', mainPage.updateBasicInfoApi);
-
 }
 
 module.exports.routes = Routes;
